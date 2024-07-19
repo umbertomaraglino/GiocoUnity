46ccc30f8880769b6fff428958d5c87fcd5aa159
umbertomaraglino <s1093420@studenti.univpm.it>
2024-07-19 11:24:59 +0200

    aggiunta funzione per aprire shop a NewMenu

--- Assets/Scripts/NewMenu.cs
+++ Assets/Scripts/NewMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 
 public class NewMenu : MonoBehaviour
@@ -26,6 +27,23 @@ public class NewMenu : MonoBehaviour
         SceneManager.LoadScene("NewMenu");
         Time.timeScale = 1f;
         //SceneManager.UnloadScene(sceneName: "SampleScene");
+    }
+
+    public void OnShop(InputAction.CallbackContext context){
+        Debug.Log("cliccato");
+        Vector2 attackPosition = transform.position; // Center of the attack
+        Vector2 attackSize = new Vector2(10f, 10f); // Size of the rectangle (width x height)
+        float attackAngle = 0f; // Angle of the rectangle, if needed
+        // Perform the overlap box check
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPosition, attackSize, attackAngle);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            if (enemy.CompareTag("Shop") && context.performed)
+            {
+                canvas_shop.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
     }
 
 }
